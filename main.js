6b8ed3f5df7aec6c76b3777afd9a80b11ced1f26
Rapha2002 <rapha.koekkoek@gmail.com>
2025-01-19 22:32:52 +0100

    Interactie toon/verberg vacatures

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-let HideButton = document.querySelector('.show-more');
+
+
+let showButton = document.querySelector('.btn');
+let containerText = document.querySelector('.info');
+
+showButton.addEventListener('click', function() {
+    containerText.classList.toggle('show-info');
+});
